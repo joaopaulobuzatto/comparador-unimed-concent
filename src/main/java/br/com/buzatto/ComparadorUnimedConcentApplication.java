dc172bb3f94c9c29fd4c9bfdc5b5f671b3929eb2
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-03-20 13:10:30 -0300

    start project

---
+++ src/main/java/br/com/buzatto/ComparadorUnimedConcentApplication.java
@@ -0,0 +1,13 @@
+package br.com.buzatto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparadorUnimedConcentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComparadorUnimedConcentApplication.class, args);
+    }
+
+}
